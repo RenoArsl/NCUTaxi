58a74b5f1c7046b3f6646fcdc5ef3f231f5225ca
RenoArsl <100261215+RenoArsl@users.noreply.github.com>
2022-05-13 12:56:48 +0800

    upgrade

--- database.php
+++ database.php
@@ -6,15 +6,8 @@
     $username = getenv('username');
     $password = getenv('password');
 
-      try {
-        $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Error " . mysqli_error($conn));
-        if ($error = mysqli_connect_error()) {
-            echo '<pre>' . print_r($error, TRUE) . '</pre>';
-        }
-          // ERRMODE_EXCEPTION: 拋出 exceptions 異常。ATTR_ERRMODE：錯誤報告。
-      } catch (PDOException $e){
-
-        echo "Connected Failed: " . $e->getMessage();
-      }
-
+    $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Error " . mysqli_error($conn));
+    if ($error = mysqli_connect_error()) {
+      echo '<pre>' . print_r($error, TRUE) . '</pre>';
+    }
 ?>
