a7b488327c28cc92babb74e92e673d23d4a4f3b7
RenoArsl <100261215+RenoArsl@users.noreply.github.com>
2022-05-13 12:47:38 +0800

    upgrade

--- database.php
+++ database.php
@@ -7,7 +7,7 @@
     $password = getenv('password');
 
       try {
-        $conn = new mysqli($servername, $username, $password, $dbname);
+        $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Error " . mysqli_error($conn));
           
           // ERRMODE_EXCEPTION: 拋出 exceptions 異常。ATTR_ERRMODE：錯誤報告。
       } catch (PDOException $e){
