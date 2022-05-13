895ecd128cdf4d5e5847157fa98426a155126f75
RenoArsl <100261215+RenoArsl@users.noreply.github.com>
2022-05-13 12:53:02 +0800

    upgrade

--- database.php
+++ database.php
@@ -8,7 +8,9 @@
 
       try {
         $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Error " . mysqli_error($conn));
-          
+        if ($error = mysqli_connect_error()) {
+            echo '<pre>' . print_r($error, TRUE) . '</pre>';
+        }
           // ERRMODE_EXCEPTION: 拋出 exceptions 異常。ATTR_ERRMODE：錯誤報告。
       } catch (PDOException $e){
 
